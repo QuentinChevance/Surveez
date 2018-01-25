9b7893897b24978a6472998e6dc76fef65df0ccf
Unknown <quentinchevance22@gmail.com>
2018-01-25 14:43:43 +0100

    Initialisation du webcomponent et test page index

--- projet-p2-react/src/App.js
+++ projet-p2-react/src/App.js
@@ -1,21 +1,46 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import '../node_modules/material-components-web/dist/material-components-web';
+import mdcAutoInit from '@material/auto-init';
+import {MDCTextField} from '@material/textfield';
+
+import './general.js';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+class App extends React.Component {
+    componentDidMount(){
+        mdcAutoInit.register('MDCTextField', MDCTextField);
+    }
+    render() {
+        return (
+
+              <body className="container container--center home">
+              <div className="card">
+                  <h2 className="mdc-typography--display3">Surveez</h2>
+                  <div className="mdc-text-field" data-mdc-auto-init="MDCTextField">
+                      <input type="text" className="mdc-text-field__input" id="email"/>
+                      <label htmlFor="email" className="mdc-text-field__label">E-mail</label>
+                  </div>
+                  <div className="mdc-text-field" data-mdc-auto-init="MDCTextField">
+                      <input type="text" className="mdc-text-field__input" id="emailconfirm"/>
+                      <label htmlFor="emailconfirm" className="mdc-text-field__label">Confirmez votre e-mail</label>
+                  </div>
+                  <div className="mdc-text-field" data-mdc-auto-init="MDCTextField">
+                      <input type="password" className="mdc-text-field__input" id="password"/>
+                      <label htmlFor="password" className="mdc-text-field__label">Mot de passe</label>
+
+                  </div>
+                  <div className="mdc-text-field" data-mdc-auto-init="MDCTextField">
+                      <input type="password" className="mdc-text-field__input" id="passwordconfirm"/>
+                      <label htmlFor="passwordconfirm" className="mdc-text-field__label">Confirmez votre mot de passe</label>
+                  </div>
+
+                  <button type="button" className="mdc-button mdc-button--raised">
+                      Inscription
+                  </button>
+              </div>
+              </body>
+        );
+    }
 }
 
 export default App;
