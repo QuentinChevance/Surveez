52b355764af7d44087d02724680ded7b98dc887e
onoma <ygdrasill84@gmail.com>
2018-01-26 16:23:51 +0100

    Auto stash before merge of "master" and "origin/master"
    Ajout du component pour le Header

--- projet-p2-react/src/App.js
+++ projet-p2-react/src/App.js
@@ -4,11 +4,15 @@ import '../node_modules/material-components-web/dist/material-components-web';
 import RegistrationForm from "./components/forms/registrationForm";
 
 import './App.css';
+import {Header} from "./components/Header/Header";
 
 class App extends React.Component {
     render() {
         return (
+            <div>
             <RegistrationForm />
+                <Header/>
+            </div>
         );
     }
 }
