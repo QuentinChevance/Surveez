3a82a559604109b1bdad31f95f1e7b7af4a16f64
Unknown <quentinchevance22@gmail.com>
2018-01-26 16:31:11 +0100

    changement getelementbyid par setstatechange

--- projet-p2-react/src/components/forms/registrationForm.js
+++ projet-p2-react/src/components/forms/registrationForm.js
@@ -3,22 +3,38 @@ import mdcAutoInit from '@material/auto-init';
 import {MDCTextField} from '@material/textfield';
 import axios from 'axios';
 
+
 class registrationForm extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            email: this.props.email,
+            firstName : this.props.firstName,
+            lastName: this.props.lastName,
+            company: this.props.company,
+            password: this.props.password
+        }
+    }
     componentDidMount(){
         mdcAutoInit.register('MDCTextField', MDCTextField);
         mdcAutoInit();
     }
 
-    onClick = () => {
-        console.log("email",document.getElementById("email"));
+    onStateChange = (event) => {
+        console.log("on est là");
+        this.setState({[event.target.id]:event.target.value});
+    };
+
+    submit = () => {
+        console.log("email",this.state.email);
         axios.post(
             `http://localhost:4000/registration`,
             {
-                email: document.getElementById("email").value,
-                firstName: document.getElementById("firstName").value,
-                lastName: document.getElementById("lastName").value,
-                company: document.getElementById("company").value,
-                password: document.getElementById("password").value
+                email: this.state.email,
+                firstName: this.state.firstName,
+                lastName: this.state.lastName,
+                company: this.state.company,
+                password: this.state.password
 
             }
         ).then(response => {
@@ -34,32 +50,32 @@ class registrationForm extends Component {
                 <div className="card">
                     <h2 className="mdc-typography--display3">Surveez</h2>
                     <div className="mdc-text-field" data-mdc-auto-init="MDCTextField">
-                        <input type="email" className="mdc-text-field__input" id="email"/>
-                        <label htmlFor="email" className="mdc-text-field__label">E-mail</label>
+                        <input type="email" className="mdc-text-field__input" onChange={this.onStateChange} id="email"/>
+                        <label htmlFor="email" className="mdc-text-field__label" >E-mail</label>
                     </div>
                     <div className="mdc-text-field" data-mdc-auto-init="MDCTextField">
-                        <input type="text" className="mdc-text-field__input" id="firstName"/>
+                        <input type="text" className="mdc-text-field__input" onChange={this.onStateChange} id="firstName"/>
                         <label htmlFor="firstName" className="mdc-text-field__label">Prénom</label>
                     </div>
                     <div className="mdc-text-field" data-mdc-auto-init="MDCTextField">
-                        <input type="text" className="mdc-text-field__input" id="lastName"/>
+                        <input type="text" className="mdc-text-field__input" onChange={this.onStateChange} id="lastName"/>
                         <label htmlFor="lastName" className="mdc-text-field__label">Nom</label>
                     </div>
                     <div className="mdc-text-field" data-mdc-auto-init="MDCTextField">
-                        <input type="text" className="mdc-text-field__input" id="company"/>
+                        <input type="text" className="mdc-text-field__input" onChange={this.onStateChange} id="company"/>
                         <label htmlFor="company" className="mdc-text-field__label">Entreprise</label>
                     </div>
                     <div className="mdc-text-field" data-mdc-auto-init="MDCTextField">
-                        <input type="password" className="mdc-text-field__input" id="password"/>
+                        <input type="password" className="mdc-text-field__input" onChange={this.onStateChange} id="password"/>
                         <label htmlFor="password" className="mdc-text-field__label">Mot de passe</label>
 
                     </div>
                     <div className="mdc-text-field" data-mdc-auto-init="MDCTextField">
-                        <input type="password" className="mdc-text-field__input" id="passwordconfirm"/>
+                        <input type="password" className="mdc-text-field__input" onChange={this.onStateChange} id="passwordconfirm"/>
                         <label htmlFor="passwordconfirm" className="mdc-text-field__label">Confirmez votre mot de passe</label>
                     </div>
 
-                    <button type="button" className="mdc-button mdc-button--raised" onClick={this.onClick}>
+                    <button type="button" className="mdc-button mdc-button--raised" onClick={this.submit}>
                         Inscription
                     </button>
                 </div>
